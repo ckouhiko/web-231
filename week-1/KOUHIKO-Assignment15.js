bb49827bb6c025f4e68c19b73758f27ae920c1c4
Comlan KOUHIKO <ckouhiko@my365.bellevue.edu>
2019-06-10 01:14:09 -0500

    Adding assignment15

---
+++ week-1/KOUHIKO-Assignment15.js
@@ -0,0 +1,50 @@
+
+    
+           
+            /*
+            ============================================
+            * Title: Assignment 1.4
+            * Author: Comlan KOUHIKO
+            * Date: June 2, 2019
+            * Modified By: 
+            * Description: Program to create 3 employee records and display them.
+            ===========================================
+            */
+            
+           
+           
+            // variables declaration:
+           var employee1 = {firstName:"Comlan", lastName:"KOUHIKO", address:"458 VanDer Lake Pl Dallas", 
+            payRate:10.215, hireDate:"05/18/2012"};
+            
+           var employee2 = {firstName:"Carlos", lastName:"GOMEZ", address:"4452 Lamar Dr Dallas", 
+           payRate: 9.65, hireDate: "07/02/2013"};
+
+           var employee3 = {firstName:"Mamy", lastName:"JOHN", address:"1879 W Lovers St Arlington", 
+           payRate: 18.875, hireDate: "11/24/2018"};
+
+
+            // Output
+
+            console.log(employee1.firstName);
+            console.log(employee1.lastName);
+            console.log(employee1.address);
+            console.log(employee1.payRate.toFixed(1));
+            console.log(employee1.hireDate);
+            console.log(" ");
+
+            console.log(employee2.firstName);
+            console.log(employee2.lastName);
+            console.log(employee2.address);
+            console.log(employee2.payRate.toFixed(1));
+            console.log(employee2.hireDate);
+            console.log(" ");
+
+            console.log(employee3.firstName);
+            console.log(employee3.lastName);
+            console.log(employee3.address);
+            console.log(employee3.payRate.toFixed(1));
+            console.log(employee3.hireDate);
+            
+            
+
