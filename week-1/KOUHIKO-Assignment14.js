b56867b4734c9c8aff6b8c4db9f35ec1f542e586
Comlan KOUHIKO <ckouhiko@my365.bellevue.edu>
2019-06-09 20:20:49 -0500

    Assignment 1.4

---
+++ week-1/KOUHIKO-Assignment14.js
@@ -0,0 +1,30 @@
+
+    
+           
+            /*
+            ============================================
+            * Title: Assignment 1.4
+            * Author: Comlan KOUHIKO
+            * Date: June 2, 2019
+            * Modified By: 
+            * Description: Variable declaration and values assignment.
+            ===========================================
+            */
+            
+
+            // variables declaration:
+            var firstName = "Comlan";
+            var lastName = "KOUHIKO";
+            var age = 54;
+            var street = "458 VanDer Lake Pl";
+            var city = "Dallas";
+            var zipCode = 75235;
+
+            // Output
+            console.log(firstName);
+            console.log(lastName);
+            console.log(age);
+            console.log(street);
+            console.log(city);
+            console.log(zipCode);
+
